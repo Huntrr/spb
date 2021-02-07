dae460dda2c2f552f8942f937418a24978452880
Hunter Lightman <git.huntrr@gmail.com>
2021-02-06 23:53:18 -0800

    basic menu page

---
+++ game/src/resources/subtitle_options.gd
@@ -0,0 +1,21 @@
+class_name SubtitleOptions
+extends Resource
+
+export(Array, String) var s = ["space"]
+export(Array, String) var p = ["pirate"]
+export(Array, String) var b = ["battle"]
+
+func get_title(not_this: String) -> String:
+	randomize()
+	var title: String = _gen_title()
+	var count: int = 0
+	while title == not_this and count < 100:
+		title = _gen_title()
+		count += 1
+	return title
+
+func _gen_title() -> String:
+	var s_ = s[randi() % s.size()]
+	var p_ = p[randi() % p.size()]
+	var b_ = b[randi() % b.size()]
+	return "%s %s %s" % [s_, p_, b_]
---
+++ game/src/resources/version_info.gd
@@ -0,0 +1,9 @@
+class_name VersionInfo
+extends Resource
+
+export(int) var major = 0
+export(int) var minor = 0
+export(int) var patch = 0
+
+export(String) var adjective
+export(String) var codename
